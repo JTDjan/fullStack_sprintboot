96ddff6575bf4090e05c15ea639650f8dbeeafe0
Your Name <you@example.com>
2022-01-14 13:59:54 +0200

    set up for api

---
+++ fullStackProject_Josh/src/main/java/com/example/fullStackProject_Josh/FullStackProjectJoshApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullStackProject_Josh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProjectJoshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackProjectJoshApplication.class, args);
+	}
+
+}
---
+++ fullStackProject_Josh/src/test/java/com/example/fullStackProject_Josh/FullStackProjectJoshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullStackProject_Josh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackProjectJoshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
